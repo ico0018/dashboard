68508e6aa3520c64f02a74a4f7a7ca86c277a83c
Siri Wang <siliwang.ico@gmail.com>
2023-10-26 11:14:20 -0400

    Add admin view

--- apps/__init__.py
+++ apps/__init__.py
@@ -5,25 +5,31 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from importlib import import_module
 
-db = SQLAlchemy()
-login_manager = LoginManager()
+from flask_admin import Admin
 
+db = SQLAlchemy()
+login_manager = LoginManager() 
 
 def register_extensions(app):
     db.init_app(app)
     login_manager.init_app(app)
 
-
 def register_blueprints(app):
     for module_name in ('authentication', 'home'):
         module = import_module('apps.{}.routes'.format(module_name))
         app.register_blueprint(module.blueprint)
-
+        
+def register_admin(app):
+    from apps.authentication.admin_view import UserAdminView
+    from apps.authentication.models import Users
+    # Initialize Flask-Admin
+    admin = Admin(app, name='Admin', template_mode='bootstrap4')
+    admin.add_view(UserAdminView(Users, db.session))
 
 def create_app(config):
     app = Flask(__name__)
     app.config.from_object(config)
     register_extensions(app)
-    
     register_blueprints(app)
+    register_admin(app)
     return app
---
+++ apps/authentication/admin_view.py
@@ -0,0 +1,16 @@
+from flask_admin.contrib.sqla import ModelView
+from flask_login import current_user
+from flask import redirect, url_for, flash
+
+class UserAdminView(ModelView):
+    column_list = ['id', 'username', 'company', 'email']
+    form_columns = ['username', 'password', 'company', 'email']
+    column_searchable_list = ['username', 'company', 'email']  # Fields that can be searched
+    column_sortable_list = ['id', 'username', 'company', 'email']  # Fields that can be sorted
+
+    def is_accessible(self):
+        return current_user.is_authenticated and current_user.role == 'admin'
+
+    def inaccessible_callback(self, name, **kwargs):
+        flash('You do not have permission to access this page.')
+        return redirect(url_for('auth.login'))
